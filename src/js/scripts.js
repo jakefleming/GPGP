41b1a803aae9592a0e2aeeabc20a43c9ca2363d9
zachalbert <zachalbert@gmail.com>
2016-04-03 23:53:55 -0700

    Add filters for sunset, day, night, and set time of day

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -87,6 +87,19 @@
     // yay
     distributeWaves();
     resetObjects();
+
+    var d = new Date();
+    var n = d.getHours();
+    if (n > 19 || n < 7) {
+      // If time is after 7PM or before 7AM, apply night theme to ‘body’
+      document.body.className = "night";
+    } else if (n > 16 && n < 19) {
+      // If time is between 4PM – 7PM sunset theme to ‘body’
+      document.body.className = "sunset";
+    } else {
+      // Else use ‘day’ theme
+      document.body.className = "day";
+    }
   });
 
   $(document).ready(function() {
