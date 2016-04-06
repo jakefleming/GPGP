f6d8bbb66db91113befd5c18fdf875772bd56017
zachalbert <zachalbert@gmail.com>
2016-04-06 02:05:43 -0700

    Final final animation tuning

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -102,7 +102,7 @@ var objects = ( function($) {
         var top   = tools.randomRange( 10, 90, 38, 68 );
         var left  = tools.randomRange( 10, 90, 38, 68 );
         var z     = Math.floor( top ) + ( $(this).height() / 8);
-        var delay = ( config.waveTiming - 1.5 ) * ( left / 100 ) * -1 + 's';
+        var delay = config.waveTiming * ( left / 100 ) * -1 + 's';
 
         $(this).css({
           'top': top + 'vh',
